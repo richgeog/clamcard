659ab560853df9db61897213c067b91d4205b2a3
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-15 14:24:24 +0100

    arrivals station/touch out test passing

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,11 @@
 class Journey
-  attr_reader :departure_station
+  attr_reader :departure_station, :arrival_station
 
   def touch_in(station)
     @departure_station = station
+  end
+
+  def touch_out(station)
+    @arrival_station = station
   end
 end
