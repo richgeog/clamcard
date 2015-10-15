d673bfb5a9e974c96ed4ba25c9a1ea73fd52da21
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-15 14:53:25 +0100

    test written for zone b stations

--- spec/zones_spec.rb
+++ spec/zones_spec.rb
@@ -6,4 +6,8 @@ describe Zones do
   it 'knows which stations are in zone A' do
     expect(zones.a).to eq "Asterisk, Antelope, Aldgate, Angel, Aardvark"
   end
+
+  it 'knows which stations are in zone B' do
+    expect(zones.b).to eq "Bison, Bugel, Balham, Bullhead, Barbican"
+  end
 end
