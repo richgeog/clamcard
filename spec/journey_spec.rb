b69f540995faa8c4f527943204a3fe21f0a0afc6
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-15 14:03:32 +0100

    added a lib file and journey class file

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,6 +1,8 @@
-subject(:journey) { Journey.new }
+require 'Journey'
 
 describe Journey do
+  subject(:journey) { Journey.new }
+
   it 'begins th journey with a departure station' do
     expect(journey).to eq "Asterisk"
   end
