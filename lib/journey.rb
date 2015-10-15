6de7619b6eb0861d5206f42f5b69851e6998eedb
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-15 14:20:21 +0100

    departure/touch n test passing

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,7 @@
 class Journey
+  attr_reader :departure_station
 
+  def touch_in(station)
+    @departure_station = station
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,8 +2,10 @@ require 'Journey'
 
 describe Journey do
   subject(:journey) { Journey.new }
+  let(:station) { double(:station, name: 'Asterisk') }
 
-  it 'begins th journey with a departure station' do
-    expect(journey).to eq "Asterisk"
+  it 'begins the journey with a departure station' do
+    journey.touch_in(station)
+    expect(journey.departure_station).to eq station
   end
 end
